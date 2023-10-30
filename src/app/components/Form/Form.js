dd26ee5fd3325e7cdc1d8bfa1ec91d4c9232e5b8
Aman Ali Saifi <amansaifi8430@gmail.com>
2023-10-30 14:28:06 +0530

    commit

--- src/app/components/Form/Form.js
+++ src/app/components/Form/Form.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Form = () => {
   return (
-    <div className="w-1280 h-250 bg-white rounded-lg mt-12 mx-16 my-12 shadow-lg mb-8 font-serif">
-      <div className="p-8">
-        <form className="space-y-6">
-          <div className="flex flex-wrap -mx-2 mb-6">
-            <div className="w-full md:w-1/2 px-2 mb-4">
-              <label htmlFor="fullName" className="text-sm text-gray-600 mb-1">
+    <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 mt-12">
+      <div className="bg-white rounded-lg shadow-lg mb-8 font-serif">
+        <div className="p-4 md:p-8">
+          <form className="space-y-6">
+            <div className="mb-6">
+              <label htmlFor="fullName" className="text-sm text-gray-600 mb-1 block">
                 Full Name
               </label>
               <input
@@ -18,58 +18,57 @@ const Form = () => {
                 required
               />
             </div>
-            <div className="w-full md:w-1/2 px-2">
-              <div className="flex flex-wrap -mx-2">
-                <div className="w-1/2 px-2 mb-4">
-                  <label htmlFor="email" className="text-sm text-gray-600 mb-1">
-                    Email address
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="py-2 px-4 border border-gray-300 w-full text-gray-800 focus:outline-none rounded"
-                    required
-                  />
-                </div>
-                <div className="w-1/2 px-2 mb-4">
-                  <label htmlFor="mobile" className="text-sm text-gray-600 mb-1">
-                    Mobile (0123456789)
-                  </label>
-                  <input
-                    type="tel"
-                    pattern="^[0-9]{10}$"
-                    id="mobile"
-                    name="mobile"
-                    className="py-2 px-4 border border-gray-300 w-full text-gray-800 focus:outline-none rounded"
-                    required
-                  />
-                </div>
+            <div className="flex flex-wrap -mx-2 mb-6">
+              <div className="w-full md:w-1/2 px-2 mb-4">
+                <label htmlFor="email" className="text-sm text-gray-600 mb-1 block">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="py-2 px-4 border border-gray-300 w-full text-gray-800 focus:outline-none rounded"
+                  required
+                />
+              </div>
+              <div className="w-full md:w-1/2 px-2 mb-4">
+                <label htmlFor="mobile" className="text-sm text-gray-600 mb-1 block">
+                  Mobile (0123456789)
+                </label>
+                <input
+                  type="tel"
+                  pattern="^[0-9]{10}$"
+                  id="mobile"
+                  name="mobile"
+                  className="py-2 px-4 border border-gray-300 w-full text-gray-800 focus:outline-none rounded"
+                  required
+                />
               </div>
             </div>
-          </div>
-          <div className="mb-6">
-            <label htmlFor="message" className="text-sm text-gray-600 mb-1">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              rows="4"
-              className="py-2 px-4 border border-gray-300 w-full text-gray-800 focus:outline-none rounded"
-              required
-            />
-          </div>
-
-          <div className="flex items-center justify-center">
+            <div className="mb-6">
+              <label htmlFor="message" className="text-sm text-gray-600 mb-1 block">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                rows="4"
+                className="py-2 px-4 border border-gray-300 w-full text-gray-800 focus:outline-none rounded"
+                required
+              />
+            </div>
+            <div className="flex items-center justify-center">
             <button
-              type="submit"
-              className="w-56 h-12 bg-blue-600 hover:bg-blue-700 text-white rounded-full transition duration-300"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
+  type="submit"
+  className="w-full md:w-auto h-8 bg-blue-600 hover:bg-blue-700 text-white rounded-lg transition duration-300 px-8"
+>
+  Submit
+</button>
+
+
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
